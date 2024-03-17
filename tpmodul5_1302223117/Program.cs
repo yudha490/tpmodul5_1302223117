21cbf098cfdbf55c886cda8c3ace73b551b1e580
yudha490 <yudhapratama8944@gmail.com>
2024-03-17 15:30:58 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302223117/Program.cs
+++ tpmodul5_1302223117/Program.cs
@@ -6,6 +6,22 @@
     }
 }
 
+public class DataGeneric<T>
+{
+    public T Data { get; set; }
+
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+    }
+}
+
+
 class Program
 {
     static void Main(string[] args)
@@ -14,5 +30,7 @@ class Program
 
         halo.SapaUser("Yudha");
 
+        DataGeneric<string> dataString = new DataGeneric<string>("1302223117");
+        dataString.PrintData();
     }
 }
--- tpmodul5_1302223117/obj/Debug/net8.0/tpmodul5_1302223117.AssemblyInfo.cs
+++ tpmodul5_1302223117/obj/Debug/net8.0/tpmodul5_1302223117.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul5_1302223117")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94b4ac029045d9fabc54ad48a6316a92d8be3252")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+902f14a772467445fc7d5ef44dd807b801a1d6a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul5_1302223117")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul5_1302223117")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
